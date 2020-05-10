5664981a9142aef0184c393c7b38a7be317ba6da
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-05-11 00:10:16 +0200

    admin example: update readme

--- examples/admin-gatsby/packages/admin/src/Layouts/menuItem.ts
+++ examples/admin-gatsby/packages/admin/src/Layouts/menuItem.ts
@@ -2,7 +2,6 @@ import { MenuItemType } from 'oah-ui';
 
 const items: MenuItemType[] = [
   { title: 'Home Page', icon: { name: 'home' }, link: { to: '/dashboard' } },
-  { title: 'FEATURES', group: true },
   {
     title: 'Models',
     icon: { name: 'layers-outline' },
@@ -20,21 +19,6 @@ const items: MenuItemType[] = [
         title: 'Groups',
         link: { to: '/models/Group' },
       },
-    ],
-  },
-  {
-    title: 'Miscellaneous',
-    icon: { name: 'shuffle-2-outline' },
-    children: [{ title: '404', link: { to: '/miscellaneous/404' } }],
-  },
-  {
-    title: 'Auth',
-    icon: { name: 'lock-outline' },
-    children: [
-      { title: 'Login', link: { to: '/auth/login' } },
-      { title: 'Register', link: { to: '/auth/register' } },
-      { title: 'Request Password', link: { to: '/auth/request-password' } },
-      { title: 'Reset Password', link: { to: '/auth/reset-password' } },
     ],
   },
 ];
