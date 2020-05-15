ec0a6769bbd102f231326130c0b1c5f015793339
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-05-15 03:19:52 +0200

    graphql-modules example: generate example schema

--- examples/graphql-modules/src/app/app.module.ts
+++ examples/graphql-modules/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { GraphQLModule } from "@graphql-modules/core";
 import { CommonModule } from "./common/common.module";
-import { PostModule } from "./Post/post.module";
-import { UserModule } from "./User/user.module";
+import { CommentModule } from "./Comment/Comment.module";
+import { PostModule } from "./Post/Post.module";
+import { GroupModule } from "./Group/Group.module";
+import { UserModule } from "./User/User.module";
 
 export const AppModule = new GraphQLModule({
-  imports: [CommonModule, UserModule, PostModule],
+  imports: [CommonModule, CommentModule, PostModule, GroupModule, UserModule],
 });
