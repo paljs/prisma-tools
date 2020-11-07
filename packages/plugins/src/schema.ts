903d8c48056dc21444f45c52246e479a9b144c21
Ahmed Elywa <ahmed.elywa@icloud.com>
2020-11-07 22:49:15 +0200

    fix issue when prisma client path not same as default

--- packages/plugins/src/schema.ts
+++ packages/plugins/src/schema.ts
@@ -1,6 +1,6 @@
 import { dmmf } from '@prisma/client';
 import { DMMF } from '@prisma/client/runtime';
 
-export const schema: DMMF.Schema = dmmf.schema;
-export const dataModel: DMMF.Datamodel = dmmf.datamodel;
+export const schema: DMMF.Schema | undefined = dmmf?.schema;
+export const dataModel: DMMF.Datamodel | undefined = dmmf?.datamodel;
 export { DMMF };
