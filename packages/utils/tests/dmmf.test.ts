45db4c743c028d0bfb4e68ed52ae7742d2a64d80
Andrew Porritt <aporritt@gogo-robot.com>
2024-07-01 15:16:04 +0100

    Don't check error message due to platform path separator differences

--- packages/utils/tests/dmmf.test.ts
+++ packages/utils/tests/dmmf.test.ts
@@ -4,7 +4,7 @@ import { getDMMFBySchemaPath, getSchemaPath } from 'dmmf';
 describe('test read schema and convert to dmmf', () => {
   const schemaPath = join(__dirname, './schemas/schema.prisma');
   test('failing load schema path', async () => {
-    await expect(getSchemaPath()).rejects.toThrowErrorMatchingSnapshot();
+    await expect(getSchemaPath()).rejects.toThrow();
   });
 
   test('success load schema path', async () => {
